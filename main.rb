3b62b6ebe145d4e82f2c8cd735636a5672fcafba
bonochof <ryos427@yahoo.co.jp>
2018-09-28 00:33:49 +0900

    add comment

--- main.rb
+++ main.rb
@@ -1,11 +1,19 @@
+# Let's begin "Zun Doko Kiyoshi" !
+
+# zun doko
 zun, doko = zundoko = ["ズン", "ドコ"]
 
+# zun counter
 count = 0
+
+# main loop
 loop do
+  # create zun or doko
   zd = zundoko[rand 2]
   print zd, " "
   sleep 1
 
+  # judge
   case zd
   when zun
     count += 1
