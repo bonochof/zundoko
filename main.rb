c8e9dfa3569b86ca365ed670bd38adf263084c61
bonochof <ryos427@yahoo.co.jp>
2020-03-06 14:30:34 +0900

    complete SE

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ Sound.register(:zun3, 'zun3.wav')
 Sound.register(:zun4, 'zun4.wav')
 Sound.register(:doko, 'doko.wav')
 Sound.register(:kiyoshi, 'kiyoshi.wav')
-Sound.register(:yabe, 'yabe.wav')
+#Sound.register(:yabe, 'yabe.wav')
 
 Window.load_resources do
   Window.bgcolor = C_BLACK
@@ -18,53 +18,62 @@ Window.load_resources do
   count = 0
   all = 0
   str = []
+  kiyoshi = false
   endflag = false
+  zd = nil
 
   # main loop
   Window.loop do
-    if !endflag
-      # create zun or doko
-      zd = zundoko[rand 2]
-      str << '' if all % 11 == 0
-      str[all / 11] += zd + ' '
-      all += 1
-      str.each_with_index do |s, i|
-        Window.draw_font(0, i * 40, s, Font.default, color: C_WHITE)
-      end
-
-      if zd == 'ズン'
-        if count == 0
-          Sound[:zun4].play
-        elsif count == 1
-          Sound[:zun2].play
+    # SE
+    if all > 0 and !endflag
+      if zd == zun
+        if count == 1
+          Sound[:zun1].play
         elsif count == 2
-          Sound[:zun3].play
+          Sound[:zun2].play
         elsif count == 3
+          Sound[:zun3].play
+        elsif count == 4
           Sound[:zun4].play
         else
-          Sound[:yabe].play
+          Sound[:zun1].play
         end
-      else
+      elsif zd == doko
         Sound[:doko].play
+      else
+        Sound[:kiyoshi].play
+        endflag = true
       end
-      sleep 1
+    end
 
-      # judge
+    if !endflag
+      # create zun or doko or kiyoshi
+      if kiyoshi
+        zd = nil
+        str[all / 11] += 'キヨシ！'
+      else
+        zd = zundoko[rand 2]
+        str << '' if all % 11 == 0
+        str[all / 11] += zd + ' '
+        all += 1
+      end
+
+      # count zun doko
       case zd
       when zun
         count += 1
       when doko
-        if count >= 4
-          str[all / 11] += 'キ!ヨ!シ!'
-          Sound[:kiyoshi].play
-          str.each_with_index do |s, i|
-            Window.draw_font(0, i * 40, s, Font.default, color: C_WHITE)
-          end
-          endflag = true
-        else
-          count = 0
-        end
+        kiyoshi = true if count >= 4
+        count = 0
       end
+
+      # display log
+      str.each_with_index do |s, i|
+        Window.draw_font(0, i * 40, s, Font.default, color: C_WHITE)
+      end
+
+      # wait 1 sec
+      sleep 0.8
     else
       str.each_with_index do |s, i|
         Window.draw_font(0, i * 40, s, Font.default, color: C_WHITE)
